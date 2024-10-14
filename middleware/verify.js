90445c9d34a29f11ba488039ed88247ec5598c37
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-10-14 14:11:05 +0530

    dummy

--- middleware/verify.js
+++ middleware/verify.js
@@ -19,7 +19,9 @@ const conditionalVerify = (request, response, next) => {
 
 const verifyUser = async (request, response, next) => {
     try {
+        console.log(request.headers)
         const authHeader = request.headers['cookie']
+        console.log(authHeader)
 
         if(!authHeader){
             return response.status(401).send({ message: 'Token not found' })
